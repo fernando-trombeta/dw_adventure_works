967ff595112198032d8b5ce5b4aafbde83037080
Fernando Trombeta <trombeta.fernando@gmail.com>
2023-01-09 15:27:00 +0000

    staging productcategory finalizada

---
+++ models/staging/stg_erp_aw__productcategory.sql
@@ -0,0 +1,18 @@
+with
+    source_productcategory as (
+        select
+            cast(productcategoryid as int) as id_product_category
+            , cast(name as string) as category_name
+            , cast(rowguid as string) as rowguid
+            , cast(modifieddate as timestamp) as modified_date
+
+            /* columns created by stitch */
+            , cast(_sdc_table_version as int) as _sdc_table_version
+            , cast(_sdc_sequence as int) as _sdc_sequence
+            , cast(_sdc_received_at as timestamp) as _sdc_received_at
+            , cast(_sdc_batched_at as timestamp) as _sdc_batched_at
+        from {{ source('erp_aw', 'productcategory') }}
+    )
+
+select *
+from source_productcategory
